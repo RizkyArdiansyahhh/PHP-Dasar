a0807163f705e6e7a7c8bf2a923f4a4c310fdfbd
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-12-17 01:19:27 +0700

    Pertemuan 1 -> comment, variabel, tipe data, operator

---
+++ pertemuan1/index.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+// Standar Ouput
+
+// echo bukan lah funsi murni tetapi sebuah konstruk
+// echo hanya menerima string, dan bisa lebih dari 1 argumen
+// echo "Hello world";
+
+// print sama halnya dengan echo, bedanya print hanya menerima satu argumen
+// print mengembalikan return value, sedangkan echo tidak
+// return value print adalah 1;
+// print "Hello world";
+
+// print_r sama halnya dengan var_dump, untuk debugging array dan object, tetapi kurang mendalam
+// print_r("Hello")
+
+// var_dump di gunakan untuk debugging semua tipe data, sama halnya dengan print_r tetapi var_dump lebih mendalam
+// mengembalikan panjang tipe data dan tipe data
+// var_dump("hello world")
+
+
+// Penulisan syntax PHP
+// 1.PHP didalam HTML
+// 2. HTML didalam PHP
+
+
+// Variabel dan tipe data
+// Variabel
+// $nama = "nama";
+// var_dump($nama);
+// echo 'nama saya adalah $nama';
+
+// Operator
+// Aritmatika
+// $a = 10;
+// $b = 20;
+// echo $a % $b
+
+
+// Concat
+$namaDepan = "Rizky";
+$namaBelakang = "Ardiansyah";
+// echo $namaDepan . " " . $namaBelakang
+echo "$namaDepan $namaBelakang";
+// 
+?>
